025ced7bcb71fca033f1e8a0f028b8fc74418583
router <zxp8466@gmail.com>
2018-05-14 15:36:30 +0800

    igmpproxy, inadyn: fix compile errors

--- trunk/user/igmpproxy/igmpproxy-0.1/src/igmpproxy.h
+++ trunk/user/igmpproxy/igmpproxy-0.1/src/igmpproxy.h
@@ -50,6 +50,7 @@
 #include <sys/time.h>
 #include <sys/ioctl.h>
 #include <sys/param.h>
+#include <sys/types.h>
 
 #include <net/if.h>
 #include <netinet/in.h>
--- trunk/user/inadyn/src/ddns.c
+++ trunk/user/inadyn/src/ddns.c
@@ -30,6 +30,7 @@
 #include <sys/ioctl.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <sys/param.h>
 #include <time.h>
 #include <unistd.h>
 
